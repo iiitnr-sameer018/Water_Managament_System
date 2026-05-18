ab0afefce8ee34d2e40f0453bfbe5072ae138942
Sameer Lakra <sameerl24102@iiitnr.edu.in>
2026-05-18 17:48:23 +0530

    Delete Water Mangament System/Water Mangament System directory

--- Water Mangament System/Water Mangament System/src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vqvnwvtwqqvgaipaeppu.supabase.co';
-const supabaseKey = 'sb_publishable_gSPFmvDojUaLMhkizDICSQ_vdFWjvGY';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
